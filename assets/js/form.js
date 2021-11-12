e0888ece94ba43529479fcbc325aa02d6e7772af
tiwaojo <tiwaojo@gmail.com>
2021-11-12 09:52:39 -0500

    team activity 3.3.1 & 3.3.2

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,35 +1,52 @@
-const emailValidation = /^(([0-9a-zA-Z])+([-._\'+&]))*[0-9a-zA-Z]+@([-0-9a-zA-Z]+[.])+[a-zA-Z]{2,6}$/;
+const emailValidation =
+  /^(([0-9a-zA-Z])+([-._\'+&]))*[0-9a-zA-Z]+@([-0-9a-zA-Z]+[.])+[a-zA-Z]{2,6}$/;
 
-const form = document.querySelector('.ud-contact-form');
-const email = document.getElementById('email');
-const submit = form.querySelector('.ud-main-btn');
+const form = document.querySelector(".ud-contact-form");
+const email = document.getElementById("email");
+const submit = form.querySelector(".ud-main-btn");
 
-function validateForm(field){
-    let value = field.value;
+function validateForm(field) {
+  let value = field.value;
 
-    if(RegExp(emailValidation).test(value)) {
-        field.classList.remove('error');
-        return true;
-    }
-    else {
-        field.classList.add('error');
-        return false;
-    }
+  const el = document.querySelector(".ud-form-group p");
+  if (el) {
+    el.parentNode.removeChild(el);
+  }
+
+  if (RegExp(emailValidation).test(value)) {
+    field.classList.remove("error");
+    return true;
+  } else {
+    field.classList.add("error");
+    return false;
+  }
 }
 
-submit.addEventListener('click', function(e) {
-    e.preventDefault();
-    const valid = validateForm(email);
-    if(valid) {
-        console.log('Success');
-    }
-    else {
-    }
+submit.addEventListener("click", function (e) {
+  e.preventDefault();
+  const valid = validateForm(email);
+
+
+  if (valid) {
+    console.log("Success");
+
+    var errEL = document.createElement("p");
+    errEL.style.color = "green";
+    var errText = document.createTextNode("Valid Input");
+    errEL.appendChild(errText);
+    submit.before(errEL);
+  } else {
+    console.log("Fail");
+    var errEL = document.createElement("p");
+    errEL.style.color = "red";
+    var errText = document.createTextNode("Invalid Input");
+    errEL.appendChild(errText);
+    submit.before(errEL);
+  }
 });
 
-email.addEventListener('input', function(e) {
-    if(e.target.classList.contains('error')) {
-        validateForm(e.target);
-    }
+email.addEventListener("input", function (e) {
+  if (e.target.classList.contains("error")) {
+    validateForm(e.target);
+  }
 });
-
